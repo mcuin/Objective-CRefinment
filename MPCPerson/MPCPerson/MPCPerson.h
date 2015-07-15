82c5333b6433e27a13c6b0bce627372d5d7f1fc3
Mykal Cuin <mykcuin@gmail.com>
2015-07-15 11:33:51 -0600

    Apple Definding Classes Exercises

---
+++ MPCPerson/MPCPerson/MPCPerson.h
@@ -0,0 +1,25 @@
+//
+//  MPCPerson.h
+//  MPCPerson
+//
+//  Created by Mykal Cuin on 7/15/15.
+//  Copyright (c) 2015 Mykal Cuin. All rights reserved.
+//
+
+#ifndef MPCPerson_MPCPerson_h
+#define MPCPerson_MPCPerson_h
+
+
+#endif
+
+@interface MPCPerson : NSObject
+
+@property NSString *fristname;
+@property NSString *lastname;
+@property NSDate *dateOfBirth;
+
+- (void)sayHello;
+
++ (void)person;
+
+@end
---
+++ MPCPerson/MPCPerson/MPCPerson.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MPCPerson
+//
+//  Created by Mykal Cuin on 7/15/15.
+//  Copyright (c) 2015 Mykal Cuin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MPCPerson.h"
+
+@implementation MPCPerson
+
+- (void)sayHello {
+    NSLog(@"Hello, world.");
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    
+}
